0e48d9aed5d1cf929fd6e71c1e5b72da521bd2d8
Kyle Szklenski <kyle.szklenski@gmail.com>
2021-08-14 07:39:26 -0400

    Prettify it

--- scenes/GUI.gd
+++ scenes/GUI.gd
@@ -3,4 +3,4 @@ extends Control
 onready var _updates_popup = $UpdatesPopup
 
 func _ready() -> void:
-    _updates_popup.show_updates(["All new accessibility options available!", "Try out the new Moderator Approval action - it's Breadtastic!", "Try the new Moderator Disapproval action!", "Morning, Jamwise Gamgees!"])
+    _updates_popup.show_updates(["All new accessibility options available! MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM", "Try out the new Moderator Approval action - it's Breadtastic!", "Try the new Moderator Disapproval action!", "Morning, Jamwise Gamgees!"])
